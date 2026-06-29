81278cf61cc7e80ba2c7ce0113aced6c0ad1066e
Tayeb Habibi <tayebhabibi1133@gmail.com>
2026-06-29 14:18:58 +0430

    political order lessons uploaded

--- lib/audio_files.dart
+++ lib/audio_files.dart
@@ -2,7 +2,7 @@ class AudioFiles {
 
 static List<String> getPlaylist(){
   List<String> list = [];
-  for (var i = 1; i < 43; i++) {
+  for (var i = 1; i < 10; i++) {
     list.add('asset/audio/Track_$i.mp3');
   }
   return list;
