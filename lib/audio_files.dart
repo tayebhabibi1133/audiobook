44f83ae739c00b49bf1231c6a38fc78b8f30be78
Tayeb Habibi <tayebhabibi1133@gmail.com>
2026-07-10 16:20:01 +0430

    new release

--- lib/audio_files.dart
+++ lib/audio_files.dart
@@ -2,7 +2,7 @@ class AudioFiles {
 
 static List<String> getPlaylist(){
   List<String> list = [];
-  for (var i = 1; i < 2; i++) {
+  for (var i = 1; i < 12; i++) {
     list.add('asset/audio/Track_$i.mp3');
   }
   return list;
