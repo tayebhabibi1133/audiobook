edaa194c1cf21aa2cb8c112a3e0909d7ab2a6a75
AboAbdullah Mohammad Tayeb Habibi <tayebhabibi1133@gmail.com>
2026-07-24 06:06:51 +0430

    changed properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:audiobook/about_page.dart';
 import 'package:audiobook/contact_page.dart';
 import 'package:audiobook/lists.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
@@ -24,6 +25,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        pageTransitionsTheme: PageTransitionsTheme(builders: {TargetPlatform.android: ZoomPageTransitionsBuilder()}),
         scaffoldBackgroundColor: Color.fromARGB(255, 251, 245, 211),
       ),
       home: MainPage(),
