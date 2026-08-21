cd344d6b485e013e1a0827d455c6391c425f86da
AboAbdullah Mohammad Tayeb Habibi <tayebhabibi1133@gmail.com>
2026-08-21 16:07:17 +0430

    second part full release

--- lib/audio_files.dart
+++ lib/audio_files.dart
@@ -2,7 +2,7 @@ class AudioFiles {
 
 static List<String> getPlaylist(){
   List<String> list = [];
-  for (var i = 1; i < 47; i++) {
+  for (var i = 1; i < 70; i++) {
     list.add('asset/audio/Track_$i.ogg');
   }
   return list;
