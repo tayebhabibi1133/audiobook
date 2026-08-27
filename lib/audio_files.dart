688daf35ba89ba198639429bbd99fcd824572e68
AboAbdullah Mohammad Tayeb Habibi <tayebhabibi1133@gmail.com>
2026-08-27 22:26:42 +0430

    audios uploaded

--- lib/audio_files.dart
+++ lib/audio_files.dart
@@ -2,8 +2,8 @@ class AudioFiles {
 
 static List<String> getPlaylist(){
   List<String> list = [];
-  for (var i = 1; i < 12; i++) {
-    list.add('asset/audio/Track_$i.mp3');
+  for (var i = 1; i < 48; i++) {
+    list.add('asset/audio/Track_$i.ogg');
   }
   return list;
 }
